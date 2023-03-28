e618c9c496c6ea8c37a5d835980942f28d2e41c2
jun02160 <jun020216@naver.com>
2023-03-28 10:31:52 +0900

    8장 React에서 리스트 렌더링하기 - Todo.js 수정

--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ListItem, ListItemText, InputBase, Checkbox} from '@material-ui/core';
 
 class Todo extends React.Component {
     constructor(props) {
@@ -6,13 +7,22 @@ class Todo extends React.Component {
         this.state = { item: props.item };  // 매개변수 item의 변수/값을 item에 대입
     }
     render() {
+        const item = this.state.item;
         return (
-            <div className='Todo'>
-                <input type="checkbox" id={this.state.item.id}  // item.id의 값으로 rendering(JPX)
-                    name={this.state.item.id} checked={this.state.item.done}  // item.done의 값으로 rendering
-                />
-                <label for={this.state.item.id}>{this.state.item.title}</label>
-            </div>
+            <ListItem>
+                <Checkbox checked={item.done} />
+                    <ListItemText>
+                        <InputBase 
+                            inputProps={{"aria-label":"naked"}}
+                            type="text"
+                            id={item.id}
+                            name={item.name}
+                            value={item.title}
+                            multiline={true}
+                            fullWidth={true}
+                            />
+                    </ListItemText>
+            </ListItem>
         );
     }
 }
