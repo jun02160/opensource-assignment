2f2e17aaac902e23042b714d4b6ba61af65f3cc6
jun02160 <jun020216@naver.com>
2023-03-28 10:55:02 +0900

    10장 React로 목록 추가,수정,삭제하기 - App.js 수정

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     };
   }
 
-  // (1) add 함수 추가
+  // add 함수 추가
   add = (item) => {
     const thisItems = this.state.items;
     item.id = "ID-"+thisItems.length;  // key를 위한 id 추가
@@ -23,6 +23,16 @@ class App extends React.Component {
     thisItems.push(item);
     this.setState({items: thisItems});  // update state
     console.log("items: ", this.state.items);
+  }
+
+  // delete 함수 추가 (alt+shift+f 포맷팅)
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items: newItems}, () => {
+      // 디버깅 콜백
+      console.log("Update Items: ", this.state.items)
+    });
   }
 
   render() {
@@ -34,13 +44,13 @@ class App extends React.Component {
       <Paper style={{margin: 16}}>
         <List>
           {this.state.items.map((item, idx) => (
-                  <Todo item={item} key={item.id}/>
+                  <Todo item={item} key={item.id} delete={item.delete}/>
           ))}
         </List>
       </Paper>
     );
 
-    // (2) add 함수 연결
+    // 생성된 Component JSX를 리턴
     return (
       <div className='App'>
         <Container maxWidth="md">
@@ -50,8 +60,6 @@ class App extends React.Component {
       </div>
     )
 
-    // 생성된 Component JSX를 리턴
-    return <div className='App'>{todoItems}</div>;
   }
 }
 export default App;
