5ab5f22fdbc1335651402d564b0f697bd4be048d
jun02160 <jun020216@naver.com>
2023-03-28 10:15:48 +0900

    8장 React에서 리스트 랜더링하기 - App.js 수정

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -6,15 +6,20 @@ class App extends React.Component {
   constructor(props) {   // 매개변수 props 생성자 
     super(props);  // 매개변수 props 초기화
     this.state = {
-      item : {id: 0, title: "Hello World 1 ", done: true},  // item에 item.id, item.title, item.done 매개변수 이름과 값을 할당
+      items : [
+        {id: 0, title: "Todo 1 ", done: false},  // item에 item.id, item.title, item.done 매개변수 이름과 값을 할당
+        {id: 1, titile: "Todo 2", done: false},
+      ],
     };
   }
   render() {
-    return (   // 매개변수 item에 변수명과 값을 전달
-      <div className='App'>
-        <Todo item={this.state.item}/>
-      </div>
-    );
+    // javascript에서 제공하는 map 함수를 이용해 배열을 반복하여 <Todo/> Component를 여러 개 생성할 수 있다.
+    var todoItems = this.state.items.map((item, idx) => (
+      <Todo item={item} key={item.id}/>
+    ));
+
+    // 생성된 Component JSX를 리턴
+    return <div className='App'>{todoItems}</div>;
   }
 }
 export default App;
